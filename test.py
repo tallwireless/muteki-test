bf71d47b8c7984fd688a86f99b7393d177b45e33
Charles Rumford <charlesr@tallwireless.com>
2021-04-16 22:36:04 -0400

    IT WORKS!

--- test.py
+++ test.py
@@ -2,31 +2,83 @@
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+import base64
 import os
+import json
+
+
+def pad(s):
+    for i in range(0, 32 - (len(s) % 32)):
+        s += " "
+    return s
+
 
 backend = default_backend()
 
-key = os.urandom(32)
-iv = os.urandom(16)
+key1 = os.urandom(32)
+iv1 = os.urandom(16)
+key2 = os.urandom(32)
+iv2 = os.urandom(16)
 
 message = input("MESSAGE BITCH:")
 print(f"Len of message: {len(message)}")
 # Pad the mesasge to the next 32 character length
-for i in range(0, 32 - (len(message) % 32)):
-    message += " "
+message = pad(message)
 
 print(f"Post Padding len of message: {len(message)}")
 print(f"message: '{message}'")
 
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-
+cipher = Cipher(algorithms.AES(key1), modes.CBC(iv1), backend=backend)
 encryptor = cipher.encryptor()
 
 ct = encryptor.update(message.encode()) + encryptor.finalize()
 
-print(ct)
+rsa_priv = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+pub = rsa_priv.public_key()
 
+wrap = {
+    "message": base64.b64encode(ct).decode(),
+    "key": base64.b64encode(
+        pub.encrypt(
+            key1 + iv1,
+            padding.OAEP(
+                padding.MGF1(algorithm=hashes.SHA256()), hashes.SHA256(), label=None
+            ),
+        )
+    ).decode(),
+}
+wrap_s = pad(json.dumps(wrap))
+
+cipher = Cipher(algorithms.AES(key2), modes.CBC(iv2), backend=backend)
+encryptor = cipher.encryptor()
+
+eline = encryptor.update(wrap_s.encode()) + encryptor.finalize()
+eline = base64.b64encode(eline)
+
+print(f" eline: '{eline}'")
+
+eline = base64.b64decode(eline)
 decryptor = cipher.decryptor()
-foobar = decryptor.update(ct) + decryptor.finalize()
+foobar = decryptor.update(eline) + decryptor.finalize()
 
 print(foobar)
+dewrap = json.loads(foobar.decode())
+
+key = base64.b64decode(dewrap["key"])
+dekey = rsa_priv.decrypt(
+    key,
+    padding.OAEP(padding.MGF1(algorithm=hashes.SHA256()), hashes.SHA256(), label=None),
+)
+
+aes_key1 = dekey[0:32]
+print(f"key: {aes_key1 }")
+aes_iv1 = dekey[32:48]
+cipher = Cipher(algorithms.AES(aes_key1), modes.CBC(aes_iv1), backend=backend)
+decryptor = cipher.decryptor()
+FUCKING_FINALLY = (
+    decryptor.update(base64.b64decode(dewrap["message"])) + decryptor.finalize()
+)
+print(FUCKING_FINALLY)
