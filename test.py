543f5b12ba31b1f63e5a70e52440d68e91cc5e24
Charles Rumford <charlesr@tallwireless.com>
2021-04-16 21:42:16 -0400

    Better print statement labels!

--- test.py
+++ test.py
@@ -10,13 +10,14 @@ key = os.urandom(32)
 iv = os.urandom(16)
 
 message = input("MESSAGE BITCH:")
-print(len(message))
-print(len(message) % 32)
+print(f"Len of message: {len(message)}")
+# Pad the mesasge to the next 32 character length
 for i in range(0, 32 - (len(message) % 32)):
     message += " "
 
-print(len(message))
+print(f"Post Padding len of message: {len(message)}")
 print(f"message: '{message}'")
+
 cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
 
 encryptor = cipher.encryptor()
